1045a62281d916fbb841993889f4163fa7144daf
SURENRA SIVAKUMAR <surenra006@gmail.com>
2026-07-09 09:51:51 +0530

    Create 168-ExcelSheetColumnTitle

---
+++ 168-ExcelSheetColumnTitle/168-ExcelSheetColumnTitle.java
@@ -0,0 +1,18 @@
+// Last updated: 7/9/2026, 9:51:50 AM
+class Solution {
+    public String convertToTitle(int columnNumber) {
+        StringBuilder result = new StringBuilder();
+
+        while (columnNumber > 0) {
+            columnNumber--;
+
+            int remainder = columnNumber % 26;
+            char ch = (char) ('A' + remainder);
+
+            result.append(ch);
+            columnNumber /= 26;
+        }
+
+        return result.reverse().toString();
+    }
+}
