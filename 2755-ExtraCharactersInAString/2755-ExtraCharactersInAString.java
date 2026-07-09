caea779ef81fab4ce407cc0aa8f6c20d93746cf7
SURENRA SIVAKUMAR <surenra006@gmail.com>
2026-07-09 09:47:51 +0530

    Create 2755-ExtraCharactersInAString

---
+++ 2755-ExtraCharactersInAString/2755-ExtraCharactersInAString.java
@@ -0,0 +1,24 @@
+// Last updated: 7/9/2026, 9:47:49 AM
+class Solution {
+    public int minExtraChar(String s, String[] dictionary) {
+        int maxLen = s.length() + 1;
+        int[] dp = new int[maxLen];
+        Arrays.fill(dp, maxLen);
+        
+        dp[0] = 0;
+        Set<String> wordSet = new HashSet<>(Arrays.asList(dictionary));
+        
+        for (int i = 1; i < maxLen; i++) {
+            dp[i] = dp[i - 1] + 1;
+            
+            for (int length = 1; length <= i; length++) {
+                String substring = s.substring(i - length, i);
+                if (wordSet.contains(substring)) {
+                    dp[i] = Math.min(dp[i], dp[i - length]);
+                }
+            }
+        }
+        
+        return dp[maxLen - 1];        
+    }
+}
