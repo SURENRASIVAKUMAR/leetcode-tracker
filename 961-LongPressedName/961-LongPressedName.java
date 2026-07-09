d60c3cb1adf9ccd0d7b81cc4109812363ed2bf06
SURENRA SIVAKUMAR <surenra006@gmail.com>
2026-07-09 09:49:12 +0530

    Create 961-LongPressedName

---
+++ 961-LongPressedName/961-LongPressedName.java
@@ -0,0 +1,15 @@
+// Last updated: 7/9/2026, 9:49:11 AM
+class Solution {
+    public boolean isLongPressedName(String name, String typed) {
+        int i = 0, n = name.length(), m = typed.length();
+        for (int j = 0; j < m; ++j) {
+            if (i < n && name.charAt(i) == typed.charAt(j)) {
+                ++i;
+            } 
+            else if (j == 0 || typed.charAt(j) != typed.charAt(j - 1)) {
+                return false; 
+            }
+        }
+        return i == n;
+    }
+}
