cb13ca2f64abefdc30402fea6951be413bab4fd8
SURENRA SIVAKUMAR <surenra006@gmail.com>
2026-07-09 09:53:38 +0530

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/9-PalindromeNumber.java
@@ -0,0 +1,17 @@
+// Last updated: 7/9/2026, 9:53:37 AM
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false; 
+
+        int temp = x; 
+        int rev = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            rev = rev * 10 + digit;
+            x = x / 10;
+        }
+
+        return temp == rev; 
+    }
+}
