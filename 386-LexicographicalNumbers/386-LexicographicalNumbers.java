403ce65d542bebfa7575182559d5fb8691a481ba
SURENRA SIVAKUMAR <surenra006@gmail.com>
2026-07-09 09:50:26 +0530

    Create 386-LexicographicalNumbers

---
+++ 386-LexicographicalNumbers/386-LexicographicalNumbers.java
@@ -0,0 +1,20 @@
+// Last updated: 7/9/2026, 9:50:25 AM
+class Solution {
+    public List<Integer> lexicalOrder(int n) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 1; i < 10; i++) {
+            dfs(i, n, result);
+        }
+        return result;
+    }
+
+    private void dfs(int current, int max, List<Integer> result) {
+        if (current > max) return;
+        result.add(current);
+        for (int i = 0; i < 10; i++) {
+            int next = current * 10 + i;
+            if (next > max) return;
+            dfs(next, max, result);
+        }
+    }
+}
