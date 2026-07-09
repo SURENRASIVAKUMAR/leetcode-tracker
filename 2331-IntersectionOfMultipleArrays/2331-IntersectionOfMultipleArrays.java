5893f07c179ae9900f9d381f5eefc3e288b97a8e
SURENRA SIVAKUMAR <surenra006@gmail.com>
2026-07-09 09:48:10 +0530

    Create 2331-IntersectionOfMultipleArrays

---
+++ 2331-IntersectionOfMultipleArrays/2331-IntersectionOfMultipleArrays.java
@@ -0,0 +1,20 @@
+// Last updated: 7/9/2026, 9:48:09 AM
+class Solution {
+    public List<Integer> intersection(int[][] nums) {
+        int freq[] = new int[1001];
+        int len = nums.length;
+        
+        for(int i = 0; i < nums.length; i++){
+            for(int j = 0; j < nums[i].length; j++){
+                freq[nums[i][j]]++;
+            }
+        }
+
+        List<Integer> list = new ArrayList<>();
+
+        for(int i = 1; i < 1001; i++){
+            if(freq[i] == len) list.add(i);
+        }
+        return list;
+    }
+}
