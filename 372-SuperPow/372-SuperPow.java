196af3e352a1b7fe4e71e4a2664194fe328ec758
SURENRA SIVAKUMAR <surenra006@gmail.com>
2026-07-09 09:50:32 +0530

    Create 372-SuperPow

---
+++ 372-SuperPow/372-SuperPow.java
@@ -0,0 +1,26 @@
+// Last updated: 7/9/2026, 9:50:30 AM
+class Solution {
+    private int binExp(int a, int b, int M){
+        int res=1;
+        a%=M;
+        while(b>0){
+            if((b&1)!=0){
+                res=(res*a)%M;
+            }
+            a=(a*a)%M;
+            b>>=1;
+        }
+        return res;
+    }
+    public int superPow(int a, int[] b) {
+        int m=1140;
+        int exp=0;
+        for(int i=0;i<b.length;i++){
+            exp=(exp*10+b[i])%m;
+        }
+        if(exp==0){
+            exp=1140;
+        }
+        return binExp(a,exp,1337);
+    }
+}
