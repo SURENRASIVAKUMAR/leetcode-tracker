b012aebf9344890dcc898876cd9a305822965ff1
SURENRA SIVAKUMAR <surenra006@gmail.com>
2026-07-09 09:52:31 +0530

    Create 81-SearchInRotatedSortedArrayIi

---
+++ 81-SearchInRotatedSortedArrayIi/81-SearchInRotatedSortedArrayIi.java
@@ -0,0 +1,11 @@
+// Last updated: 7/9/2026, 9:52:30 AM
+class Solution {
+    public boolean search(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]== target){
+                return true;
+            }
+        }
+        return false;
+    }
+}
