cf443cda8040c222ccc9e0af94ffe4034067a115
SURENRA SIVAKUMAR <surenra006@gmail.com>
2026-07-09 09:47:36 +0530

    Create 3206-FindCommonElementsBetweenTwoArrays

---
+++ 3206-FindCommonElementsBetweenTwoArrays/3206-FindCommonElementsBetweenTwoArrays.java
@@ -0,0 +1,26 @@
+// Last updated: 7/9/2026, 9:47:34 AM
+class Solution {
+    public int[] findIntersectionValues(int[] nums1, int[] nums2) {
+        int left = 0, right = 0;
+
+        for (int i : nums1) {
+            for (int j : nums2) {
+                if (i == j) {
+                    left++;
+                    break;
+                }
+            }
+        }
+
+        for (int i : nums2) {
+            for (int j : nums1) {
+                if (i == j) {
+                    right++;
+                    break;
+                }
+            }
+        }
+
+        return new int[]{left, right};
+    }
+}
