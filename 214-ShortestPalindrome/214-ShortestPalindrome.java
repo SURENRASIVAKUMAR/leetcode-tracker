f1f5d5f9fc1ce334bb3f29eaffd344a735e882d1
SURENRA SIVAKUMAR <surenra006@gmail.com>
2026-07-09 09:51:29 +0530

    Create 214-ShortestPalindrome

---
+++ 214-ShortestPalindrome/214-ShortestPalindrome.java
@@ -0,0 +1,30 @@
+// Last updated: 7/9/2026, 9:51:28 AM
+class Solution {
+    public String shortestPalindrome(String s) {
+        int length = s.length();
+        if (length == 0) {
+            return s;
+        }
+
+        int left = 0;
+        for (int right = length - 1; right >= 0; right--) {
+            if (s.charAt(right) == s.charAt(left)) {
+                left++;
+            }
+        }
+
+        if (left == length) {
+            return s;
+        }
+
+        String nonPalindromeSuffix = s.substring(left);
+        StringBuilder reverseSuffix = new StringBuilder(
+            nonPalindromeSuffix
+        ).reverse();
+
+        return reverseSuffix
+            .append(shortestPalindrome(s.substring(0, left)))
+            .append(nonPalindromeSuffix)
+            .toString();
+    }
+}
