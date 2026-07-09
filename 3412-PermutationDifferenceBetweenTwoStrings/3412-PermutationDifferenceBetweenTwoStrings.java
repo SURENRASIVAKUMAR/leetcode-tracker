1ce642d4c78d37ce8d4f648c557db701550a98ef
SURENRA SIVAKUMAR <surenra006@gmail.com>
2026-07-09 09:47:30 +0530

    Create 3412-PermutationDifferenceBetweenTwoStrings

---
+++ 3412-PermutationDifferenceBetweenTwoStrings/3412-PermutationDifferenceBetweenTwoStrings.java
@@ -0,0 +1,15 @@
+// Last updated: 7/9/2026, 9:47:29 AM
+class Solution {
+    public int findPermutationDifference(String s, String t) {
+        int ans=0;
+        for(int i=0;i<s.length();i++){
+            for(int j=0;j<t.length();j++){
+                if(s.charAt(i)==t.charAt(j)){
+                    ans+=Math.abs(i-j);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+}
