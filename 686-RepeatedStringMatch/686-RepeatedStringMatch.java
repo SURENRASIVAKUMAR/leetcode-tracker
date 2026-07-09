249d481b61e5e977c029e35cced4225761fd253f
SURENRA SIVAKUMAR <surenra006@gmail.com>
2026-07-09 09:49:32 +0530

    Create 686-RepeatedStringMatch

---
+++ 686-RepeatedStringMatch/686-RepeatedStringMatch.java
@@ -0,0 +1,15 @@
+// Last updated: 7/9/2026, 9:49:31 AM
+class Solution {
+    public int repeatedStringMatch(String a, String b) {
+        if (b.equals("")) return 0;
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+        while (sb.length() < b.length()) {
+            sb.append(a);
+            ++count;
+        }
+        if (sb.toString().contains(b)) return count;
+        sb.append(a);
+        return sb.toString().contains(b) ? ++count : -1;
+    }
+}
