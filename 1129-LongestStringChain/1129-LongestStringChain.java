0d0fd37e0a5c234f101d9100170dfc118e958bdf
SURENRA SIVAKUMAR <surenra006@gmail.com>
2026-07-09 09:49:05 +0530

    Create 1129-LongestStringChain

---
+++ 1129-LongestStringChain/1129-LongestStringChain.java
@@ -0,0 +1,19 @@
+// Last updated: 7/9/2026, 9:49:04 AM
+public class Solution {
+    public int longestStrChain(String[] words) {
+        Arrays.sort(words, (a, b) -> a.length() - b.length());
+        HashMap<String, Integer> dp = new HashMap<>();
+        int max_chain = 0;
+        for (String word : words) {
+            dp.put(word, 1);
+            for (int i = 0; i < word.length(); i++) {
+                String prev_word = word.substring(0, i) + word.substring(i + 1);
+                if (dp.containsKey(prev_word)) {
+                    dp.put(word, Math.max(dp.get(word), dp.get(prev_word) + 1));
+                }
+            }
+            max_chain = Math.max(max_chain, dp.get(word));
+        }
+        return max_chain;
+    }
+}
