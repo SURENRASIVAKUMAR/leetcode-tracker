54321fa4b2da938a576236464a78e8d5970da932
SURENRA SIVAKUMAR <surenra006@gmail.com>
2026-07-09 09:51:00 +0530

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/283-MoveZeroes.java
@@ -0,0 +1,14 @@
+// Last updated: 7/9/2026, 9:50:58 AM
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int left=0;
+        for(int right=0;right<nums.length;right++){
+            if(nums[right]!=0){
+                int temp=nums[right];
+                nums[right]=nums[left];
+                nums[left]=temp;
+                left++;
+            }
+        }
+    }
+}
