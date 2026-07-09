931d4af680536a3c127363ead5b0360d99369d68
SURENRA SIVAKUMAR <surenra006@gmail.com>
2026-07-09 09:53:07 +0530

    Create 38-CountAndSay

---
+++ 38-CountAndSay/38-CountAndSay.java
@@ -0,0 +1,31 @@
+// Last updated: 7/9/2026, 9:53:06 AM
+ public class Solution {
+    public String countAndSay(int n) {
+        String s = "1";
+        for(int i = 1; i < n; i++){
+            s = countIdx(s);
+        }
+        return s;
+    }
+    
+    public String countIdx(String s){
+        StringBuilder sb = new StringBuilder();
+        char c = s.charAt(0);
+        int count = 1;
+        for(int i = 1; i < s.length(); i++){
+            if(s.charAt(i) == c){
+                count++;
+            }
+            else
+            {
+                sb.append(count);
+                sb.append(c);
+                c = s.charAt(i);
+                count = 1;
+            }
+        }
+        sb.append(count);
+        sb.append(c);
+        return sb.toString();
+    }
+}
