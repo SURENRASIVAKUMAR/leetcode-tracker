d00bd8849b74a1637c1a6271ca57217f3449b82b
SURENRA SIVAKUMAR <surenra006@gmail.com>
2026-07-09 09:47:18 +0530

    Create 3785-FindTheNumberOfCopyArrays

---
+++ 3785-FindTheNumberOfCopyArrays/3785-FindTheNumberOfCopyArrays.java
@@ -0,0 +1,20 @@
+// Last updated: 7/9/2026, 9:47:17 AM
+class Solution {
+    public int countArrays(int[] original, int[][] bounds) {
+        int left = bounds[0][0];
+        int right = bounds[0][1];
+
+        for (int i = 1; i < original.length; i++) {
+            int diff = original[i] - original[i - 1];
+
+            left = Math.max(left + diff, bounds[i][0]);
+            right = Math.min(right + diff, bounds[i][1]);
+
+            if (left > right) {
+                return 0;
+            }
+        }
+
+        return right - left + 1;
+    }
+}
