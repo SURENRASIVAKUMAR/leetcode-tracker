19e133e394b778442aa570f0fe7d8df0a4b7b1d5
SURENRA SIVAKUMAR <surenra006@gmail.com>
2026-09-02 10:17:40 +0530

    Create 4242-SumOfGcdOfFormedPairs

---
+++ 4242-SumOfGcdOfFormedPairs/4242-SumOfGcdOfFormedPairs.java
@@ -0,0 +1,31 @@
+// Last updated: 9/2/2026, 10:17:25 AM
+import java.util.*;
+class Solution {
+    public int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+    public long gcdSum(int[] nums) {
+        int n = nums.length;
+        int[] prefixGcd = new int[n];
+        int mx = nums[0];
+        for (int i = 0; i < n; i++) {
+            mx = Math.max(mx, nums[i]);
+            prefixGcd[i] = gcd(nums[i], mx);
+        }
+        Arrays.sort(prefixGcd);
+        long sum = 0;
+        int l = 0;
+        int r = n - 1;
+        while (l < r) {
+            sum += gcd(prefixGcd[l], prefixGcd[r]);
+            l++;
+            r--;
+        }
+        return sum;
+    }
+}
