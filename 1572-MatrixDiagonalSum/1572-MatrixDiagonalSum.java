e22f686c4aa2ae78deef463691f93562bbad3366
SURENRA SIVAKUMAR <surenra006@gmail.com>
2026-09-03 14:18:07 +0530

    File updated at 9/3/2026, 2:17:48 PM

--- 1572-MatrixDiagonalSum/1572-MatrixDiagonalSum.java
+++ 1572-MatrixDiagonalSum/1572-MatrixDiagonalSum.java
@@ -1,15 +1,29 @@
-// Last updated: 9/3/2026, 2:08:14 PM
+// Last updated: 9/3/2026, 2:17:48 PM
 1class Solution {
-2    public int countNegatives(int[][] grid) {
-3        int c=0;
-4        for(int i=0;i<grid.length;i++){
-5            for(int j=0;j<grid[i].length;j++){
-6                 if(grid[i][j]<0){
-7                c++;
-8                }
-9            }
-10           
-11        }
-12        return c;
-13    }
-14}
+2    public List<Integer> luckyNumbers (int[][] matrix) {
+3        List<Integer> ans = new ArrayList<>();
+4        int rows = matrix.length;
+5        int cols = matrix[0].length;
+6        for (int i = 0; i < rows; i++) {
+7            int min = matrix[i][0];
+8            int col = 0;
+9            for (int j = 0; j < cols; j++) {
+10                if (matrix[i][j] < min) {
+11                    min = matrix[i][j];
+12                    col = j;
+13                }
+14            }
+15            boolean lucky = true;
+16            for (int j = 0; j < rows; j++) {
+17                if (matrix[j][col] > min) {
+18                    lucky = false;
+19                    break;
+20                }
+21            }
+22            if (lucky) {
+23                ans.add(min);
+24            }
+25        }
+26        return ans;
+27    }
+28}
