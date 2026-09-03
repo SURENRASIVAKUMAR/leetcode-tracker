798f69fdd9d3ae2f71921a74a79c90439364226b
SURENRA SIVAKUMAR <surenra006@gmail.com>
2026-09-03 14:50:06 +0530

    File updated at 9/3/2026, 2:49:47 PM

--- 1572-MatrixDiagonalSum/1572-MatrixDiagonalSum.java
+++ 1572-MatrixDiagonalSum/1572-MatrixDiagonalSum.java
@@ -1,13 +1,23 @@
-// Last updated: 9/3/2026, 2:28:58 PM
+// Last updated: 9/3/2026, 2:49:47 PM
 1class Solution {
-2    public boolean isToeplitzMatrix(int[][] matrix) {
-3        for (int i = 0; i < matrix.length - 1; i++) {
-4            for (int j = 0; j < matrix[i].length - 1; j++) {
-5                if (matrix[i][j] != matrix[i + 1][j + 1]) {
-6                return false;
-7                }
-8            }
-9        }
-10        return true;
-11    }
-12}
+2    public void rotate(int[][] matrix) {
+3        int n = matrix.length;
+4        for (int i = 0; i < n; i++) {
+5            for (int j = i + 1; j < n; j++) {
+6                int temp = matrix[i][j];
+7                matrix[i][j] = matrix[j][i];
+8                matrix[j][i] = temp;
+9            }
+10        }
+11        for (int i = 0; i < n; i++) {
+12            int l = 0, r = n - 1;
+13            while (l < r) {
+14                int temp = matrix[i][l];
+15                matrix[i][l] = matrix[i][r];
+16                matrix[i][r] = temp;
+17                l++;
+18                r--;
+19            }
+20        }
+21    }
+22}
