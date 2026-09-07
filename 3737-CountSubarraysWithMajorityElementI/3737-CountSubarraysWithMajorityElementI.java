1083a7a334dc0cde764fa84e513c73b11e09ae32
SURENRA SIVAKUMAR <surenra006@gmail.com>
2026-09-07 13:42:22 +0530

    Create 3737-CountSubarraysWithMajorityElementI

---
+++ 3737-CountSubarraysWithMajorityElementI/3737-CountSubarraysWithMajorityElementI.java
@@ -0,0 +1,27 @@
+// Last updated: 9/7/2026, 1:41:56 PM
+class Solution {
+    public int countMajoritySubarrays(int[] nums, int target) {
+
+        int ans = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int count = 0;
+
+            for (int j = i; j < nums.length; j++) {
+
+                if (nums[j] == target) {
+                    count++;
+                }
+
+                int len = j - i + 1;
+
+                if (count > len / 2) {
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
