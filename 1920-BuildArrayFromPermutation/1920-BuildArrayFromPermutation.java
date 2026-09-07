3c5e9035afff4a748e6f9e415e6866a278bcb8fc
SURENRA SIVAKUMAR <surenra006@gmail.com>
2026-09-07 13:46:01 +0530

    Create 1920-BuildArrayFromPermutation

---
+++ 1920-BuildArrayFromPermutation/1920-BuildArrayFromPermutation.java
@@ -0,0 +1,11 @@
+// Last updated: 9/7/2026, 1:45:35 PM
+class Solution {
+    public int[] buildArray(int[] nums) {
+        
+        int[] ans=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            ans[i]=nums[nums[i]];
+        }
+        return ans;
+    }
+}
