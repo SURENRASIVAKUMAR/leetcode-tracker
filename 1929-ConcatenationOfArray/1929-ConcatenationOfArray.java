0ebe1d8d5feecb57e84e55350f8b7f0ee8501e7f
SURENRA SIVAKUMAR <surenra006@gmail.com>
2026-09-07 13:45:58 +0530

    Create 1929-ConcatenationOfArray

---
+++ 1929-ConcatenationOfArray/1929-ConcatenationOfArray.java
@@ -0,0 +1,12 @@
+// Last updated: 9/7/2026, 1:45:32 PM
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[2 * n];
+        for(int i = 0; i < n; i++) {
+            ans[i] = nums[i];
+            ans[i + n] = nums[i];
+        }
+        return ans;
+    }
+}
