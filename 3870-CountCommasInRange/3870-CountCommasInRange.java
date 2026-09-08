727951145e410c1e921815f413a16a7f86c657c2
SURENRA SIVAKUMAR <surenra006@gmail.com>
2026-09-08 18:26:06 +0530

    Create 3870-CountCommasInRange

---
+++ 3870-CountCommasInRange/3870-CountCommasInRange.java
@@ -0,0 +1,14 @@
+// Last updated: 9/8/2026, 6:25:37 PM
+class Solution {
+    public int countCommas(int n) {
+        int count = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (i >= 1000) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
